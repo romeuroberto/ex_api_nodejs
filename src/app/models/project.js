de6a86146027f3c4ea1b36573c8483437c9da32a
romeuroberto <robertoromeu2009@gmail.com>
2022-06-05 10:43:40 -0300

    Creation of new models

---
+++ src/app/models/project.js
@@ -0,0 +1,29 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('../../database');
+
+const projetcSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    tasks: [{
+        type: mongoose.Schema.Types.ObjectId,
+        REF: 'Task'
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Project = mongoose.model('Project', projectSchema);
+module.exports = Project;
---
+++ src/app/models/task.js
@@ -0,0 +1,31 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('../../database');
+
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true,
+    },
+    project: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Project',
+        required: true,
+    },
+    assingnedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    completed: {
+        type:Boolean,
+        required: true,
+        default: false,
+    }, 
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Project = mongoose.model('Task', taskSchema);
+module.exports = Task;
